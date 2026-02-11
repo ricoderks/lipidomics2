f61067fdd6c74e88d53f913a3350095290278e57
Rico Derks <r.j.e.derks@lumc.nl>
2026-02-11 14:37:01 +0100

    Heatmap: adjust color scale for the feature annotation.

--- R/fct_heatmap.R
+++ R/fct_heatmap.R
@@ -30,6 +30,20 @@ show_heatmap <- function(data = NULL,
   columns <- c("my_id", "sample_name", "LongLipidName",
                "ShortLipidName", "Class")
 
+  colors <- c("#F81626", "#32E322", "#1C0DFC", "#EDB8C7", "#FF22EC", "#0DD7FD",
+              "#D7C500", "#006535", "#E97D35", "#5D3287", "#CD0071", "#845C16",
+              "#F99FFE", "#C400FF", "#00DDBE", "#759EFD", "#8A1C63", "#7390A3",
+              "#99D576", "#F28293", "#B3BE9F", "#FE68CB", "#5F4549", "#BE1C16",
+              "#AC00B6", "#C6BAFB", "#BB7DFD", "#FAB980", "#0069A1", "#FF0079",
+              "#808722", "#1CDE86", "#930D2A", "#2E38B2", "#9F75A0", "#FBB900",
+              "#E98AC3", "#974F51", "#79D4D8", "#008200", "#8DD700", "#76C495",
+              "#FF16B8", "#555626", "#0D79FC", "#8000DC", "#D9C580", "#963B96",
+              "#BD6D40", "#FC6280")
+
+  classes <- sort(unique(data$Class))
+  colors <- colors[1:length(classes)]
+  names(colors) <- classes
+
   data <- data[order(data$Class, data$LongLipidName), ]
 
   data_wide <- data[, c(columns, area_column)] |>
@@ -66,6 +80,7 @@ show_heatmap <- function(data = NULL,
     labRow = data_wide$ShortLipidName,
     col_side_colors = sample_annotation,
     row_side_colors = feature_annotation,
+    row_side_palette = colors,
     scale = "row"
   )
 
